218b8d937fd58e030f482f3ab7626f1ad029f1f8
suys <yongssu163.com>
2015-07-26 15:48:37 +0800

    json文档完善

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # json
-json是一个C++语言的Json解析程序，提供json解析及构造json数据功能。
+json是一个C++语言的Json解析器，提供json解析及构造json数据功能。
+(目前只支持英文的解析，支持中文的构造）
+
+#json解析器解析json例
+	ifstream fin("test.json");
+	string str;
+	Json json(&fin);      //开始解析
+	json.writeToStr(str); //如果解析成功，打印出解析出来的json字符，失败则报出异常
+ 	cout << str << endl;  
+	cout << endl;
+	string str1;
+	json.getRoot()->writeToStr(str1);  //读取json元素
+	cout << str1 << endl;	
+	string str2;   //依次读取元素
+	json.getRoot()->toDict()->getValue("key4")->toList()->getValue(1)->writeToStr(str2);
+	cout << str2 << endl;`
+#json解析器构造json文本样例
+	Json json;
+	JsonDict dict1;
+	//构造元素
+	JsonValue value1(123.0);
+	JsonValue value2(456.0);	
+	JsonList value3;
+	JsonValue val1(12.0);
+	JsonValue val2(34.0);
+	value3.append(&val1);
+	value3.append(&val2);
+	//把元素加入对象
+	dict1.setValue("key1", &value1);
+	dict1.setValue("key2", &value2);
+	dict1.setValue("key3", &value3);
+	//把dict1设为根节点
+	json.setRoot(&dict1);
+	//打印构造的json字符串
+	string str;
+	json.writeToStr(str);
+	cout << str << endl;
--- json/main.cpp
+++ json/main.cpp
@@ -10,33 +10,34 @@ int main()
 	/*
 	ifstream fin("test.json");
 	string str;
-	Json json(&fin);
-	json.writeToStr(str);
-	cout << str << endl;
+	Json json(&fin);      //开始解析
+	json.writeToStr(str); //如果解析成功，打印出解析出来的json字符，失败则报出异常
+	cout << str << endl;  
 	cout << endl;
 	string str1;
-	json.getRoot()->writeToStr(str1);
-	cout << str1 << endl;
-	//依次读取元素
-	string str2;
+	json.getRoot()->writeToStr(str1);  //读取json元素
+	cout << str1 << endl;	
+	string str2;   //依次读取元素
 	json.getRoot()->toDict()->getValue("key4")->toList()->getValue(1)->writeToStr(str2);
 	cout << str2 << endl;*/
 	
 	Json json;
-	JsonValue value1(123.0);
 	JsonDict dict1;
-	dict1.setValue("key1", &value1);
-	JsonValue value2(456.0);
-	dict1.setValue("key2", &value2);
-	json.setRoot(&dict1);
+	//构造元素
+	JsonValue value1(123.0);
+	JsonValue value2(456.0);	
 	JsonList value3;
 	JsonValue val1(12.0);
 	JsonValue val2(34.0);
 	value3.append(&val1);
 	value3.append(&val2);
+	//把元素加入对象
+	dict1.setValue("key1", &value1);
+	dict1.setValue("key2", &value2);
 	dict1.setValue("key3", &value3);
-	JsonString str1("hello world");
-	dict1.setValue("key4", &str1);
+	//把dict1设为根节点
+	json.setRoot(&dict1);
+	//打印构造的json字符串
 	string str;
 	json.writeToStr(str);
 	cout << str << endl;
